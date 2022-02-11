248ed0da0ec0e4d922b7233b2547513610fe3cf8
Daniel <daniel.danielarchibald@gmail.com>
2022-02-10 23:38:23 -0500

    add videoId

--- api.js
+++ api.js
@@ -7,59 +7,32 @@ const { append } = require('express/lib/response');
 function getYouTubeTrending() {
     //https://www.npmjs.com/package/request
     request('http://www.youtube.com/trending', function (error, response, body) {
-        
-        let list = body.split(`"watchEndpoint":{"videoId":`)
-
+    //      http://www.youtube.com/trending > returns HTML body
         let trending = []
-            for (var i = 0; i < list.length - 1; ++i) {
-                 //videoId
-                 //if(i != 0) 
-                // {console.log(list[i].slice(0,13));}
-                 //videoInfo
-                 
-                  var chunk = list[i].slice(list[i].length - 1500, list[i].length)
-                 
-                   var json = "{" + chunk.slice(chunk.indexOf(`"title"`),chunk.indexOf(`,"descriptionSnippet"`)) + "}"
-                   if (json.includes(`{"runs":[{"text"`)){
-                    if (json.slice(json.length - 4, json.length) != "}}}}"){
-                        trending.push(JSON.parse(json + "}"))
-                    } else {
-                        trending.push(JSON.parse(json))
-                    }
+        let list = body.split(`"watchEndpoint":{"videoId":"`) // splitting by this value gives an even 99 segments of HTML,
+        //all of which start with a video ID: www.youtube.com/watch?v=Dgdfglskndfg  <-
+        
+        for (var i = 0; i < list.length - 1; ++i) {
+            //the video id is the first few chars of every element in this list.
+            var videoId = list[i].slice(0,11)
+            //each video's meta data is roughly the last 1500 chars of each element.
+            var chunk = list[i].slice(list[i].length - 1500, list[i].length)
+            var json = "{" + chunk.slice(chunk.indexOf(`"title"`), chunk.indexOf(`,"descriptionSnippet"`)) + "}"
+            
+            if (json.includes(`{"runs":[{"text"`)) {
+                (json.slice(json.length - 4, json.length) != "}}}}") ?
+                    trending.push(Object.assign(JSON.parse(json + "}"), {"videoId": videoId})) :
+                    trending.push(Object.assign(JSON.parse(json), {"videoId": videoId}))
 
-                   }
             }
-       
-
-            server.listen(3000, () => {
-                console.log("running")
-
-                server.use("/",(req,res) => {
-                    //res.send(JSON.parse(json))
-                    res.send(trending)
-                })
-            })
         }
-    )}
-getYouTubeTrending();
 
-//         fs.writeFile("foo.html",body, () => '')
-    //         // while (match= /{"label"/g.exec(body))
-    //         //     indexes.push([match.index, match.index+match[0].length]);
-    //         //console.log(body.substring( 318166, 318545,))
-    //         // while ( (result = regex.exec(str)) ) {
-    //         //     indices.push(result.index);
-    //         //     console.log(indices)
-    //         // }
-    //         //console.log(str.matchAll(regex))
-    //         // console.log(indices)
-    //        let list = body.split(`"watchEndpoint":{"videoId":`)
-    //        // "watchEndpoint":{"videoId"
-            
-    //         list.forEach(element => {
-    //             console.log(element.slice(0,500));
-    //         });
-    //     //        console.log(i)
-    //     //    }
-    //         //console.log(Array.from(body.matchAll(regex)));
-            
+        server.listen(3000, () => {
+            console.log("running")
+
+            server.use("/", (req, res) => res.send(trending))
+        })
+    })
+}
+
+getYouTubeTrending();
--- regex.js
+++
@@ -1,5 +0,0 @@
-str = "}}}}"
-
-if (str.slice(str.length - 4, str.length) != "}}}}"){
-    console.log("doesnt match")
-}
